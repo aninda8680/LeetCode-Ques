37cac8db6da3e34d1908321560c618c3a023fce3
aninda8680 <anindadebta8680@gmail.com>
2025-07-23 23:30:52 +0530

    updated

---
+++ Numbers_Disappeared_Array.py
@@ -0,0 +1,7 @@
+def findDisappearedNumbers(nums):
+    not_p = []
+    for i in range (1, len(nums)+1):
+        if i not in nums:
+            not_p.append(i)
+    return not_p
+print(findDisappearedNumbers([4,3,2,7,8,2,3,1]))
