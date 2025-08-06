7c7522fe4c3910afb773746a01a68b4654598369
aninda8680 <anindadebta8680@gmail.com>
2025-08-06 11:49:27 +0530

    3Sum

--- 15.py
+++ 15.py
@@ -21,3 +21,32 @@ class Solution(object):
                         right -=1
         return final 
         
+
+
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        final = []
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]: continue
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
+                if s > 0:
+                    right -= 1
+                elif s < 0:
+                    left += 1
+                else:
+                    final.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -=1
+        return final 
+        
