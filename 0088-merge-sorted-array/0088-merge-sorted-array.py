a41874aa44cbb4accc83dbc767b7dc3fd4c856bb
Aninda Debta <144764740+aninda8680@users.noreply.github.com>
2025-11-18 02:46:09 +0530

    Time: 0 ms (100%), Space: 12.3 MB (84.29%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None. Modify nums1 in-place.
+        """
+
+        # Pointers for nums1, nums2, and the end of nums1
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+
+        # Merge from the back
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+        # If nums2 still has elements left
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
